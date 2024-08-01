32b0e8aeb17299ccacbd82c393ae90b863f6c737
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-01 08:51:50 +0300

    feat:started the section of lacations

--- script.js
+++ script.js
@@ -27,3 +27,20 @@ showMenu(menuBtn, sectionName, heroSection);
 showMore(moreBtn, sectionName, heroSection);
 showForm(showFormBtn, applicationForm);
 closeForm(applicationForm, btnCloseForm);
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude: lat, longitude: lng } = position.coords;
+    const cordinates = `   https://www.google.com/maps/@${lat},${lng},10z?entry=ttu   `;
+    console.log(cordinates);
+
+    const map = L.map("map").setView([lat, lng], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([lat, lng]).addTo(map).bindPopup("codesmann").openPopup();
+  },
+  (err) => alert(err)
+);
