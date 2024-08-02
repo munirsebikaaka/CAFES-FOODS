121274b7a430ee666525ccf574fb6268faac6bad
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-02 12:28:25 +0300

    feat:emplemented the functionallity of the loging in of the application

--- script.js
+++ script.js
@@ -27,23 +27,81 @@ showMore(moreBtn, sectionName, heroSection);
 showForm(showFormBtn, applicationForm);
 closeForm(applicationForm, btnCloseForm);
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    const { latitude: lat, longitude: lng } = position.coords;
-    const coord = [lat, lng];
-    console.log(`https://www.google.com/maps/@${lat},${lng}`);
+//REGSTER AND LOGIN FUNCTIONALLITY OF THE APPLICATION
+const firstNames = document.querySelector(".first-name");
+const lastNames = document.querySelector(".last-name");
+const dateOfBirths = document.querySelector(".dates");
+const emails = document.querySelector(".emails");
+const passwords = document.querySelector(".password");
+const comfirmPasswords = document.querySelector(".comfirm");
+const welcomeMsg = document.querySelector(".welcome");
+const comfirmBTN = document.querySelector(".comfirm-btn");
 
-    const map = L.map("map").setView([51.505, -0.09], 13);
+const client = {
+  firstName: [],
+  lastName: [],
+  dateOfBirth: [],
+  email: [],
+  password: [],
+};
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+function regsiter() {
+  if (firstNames.value) {
+    client.firstName.push(firstNames.value);
+  } else {
+    alert("Please input first name!");
+  }
+  if (lastNames.value) {
+    client.lastName.push(lastNames.value);
+  } else {
+    alert("please input last name!");
+  }
+  if (dateOfBirths.value) {
+    client.dateOfBirth.push(dateOfBirths.value);
+  } else {
+    alert("please input date of birth!");
+  }
+  if (emails.value) {
+    client.email.push(emails.value);
+  } else {
+    alert("Please input your email address!");
+  }
+  if (passwords.value.length > 3) {
+    if (passwords.value && passwords.value === comfirmPasswords.value) {
+      client.password.push(passwords.value);
+    } else {
+      alert("Please input password!");
+    }
+  } else {
+    alert("too short");
+  }
+  console.log(client);
+}
 
-    L.marker([51.5, -0.09])
-      .addTo(map)
-      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-      .openPopup();
-  },
-  () => alert("error")
-);
+comfirmBTN.addEventListener("click", () => {
+  regsiter();
+  if (
+    firstNames.value &&
+    lastNames.value &&
+    dateOfBirths.value &&
+    emails.value &&
+    passwords.value &&
+    passwords.value === comfirmPasswords.value
+  ) {
+    welcomeMsg.textContent = `welcome mr ${firstNames.value}`;
+  }
+});
+const accEmail = document.querySelector(".login-email");
+const accPassword = document.querySelector(".login-password");
+const btnLoginOldAcc = document.querySelector(".login-old-acc");
+
+function loginOldAccount() {
+  const oldEmail = client.email;
+  const oldPassword = client.password;
+  oldEmail.forEach((em, i) => {
+    if (accEmail.value === em && oldPassword[i] === accPassword.value) {
+      alert("CORECT!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+  });
+}
+btnLoginOldAcc.addEventListener("click", loginOldAccount);
